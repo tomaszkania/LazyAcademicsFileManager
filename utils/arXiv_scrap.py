72ea7ff6b639394634e92344097748ac7df3b8fa
tomaszkania <33237256+tomaszkania@users.noreply.github.com>
2023-04-29 19:37:09 +0200

    Update arXiv_scrap.py
    docstring added

--- utils/arXiv_scrap.py
+++ utils/arXiv_scrap.py
@@ -5,6 +5,43 @@ import re
 from typing import Dict, List
 
 class ArxivScraper:
+    """
+    A class designed to search, list, and download arXiv preprints based on specified search parameters.
+
+    The `ArxivScraper` class provides functionality to search for arXiv preprints by author, title,
+    year range, and other specified criteria. It allows users to list the preprints matching the search
+    parameters and download their PDFs.
+
+    Attributes:
+        base_url (str): The base URL for the arXiv API.
+        search_url (str): The URL for the arXiv API search endpoint.
+        rows (int): The maximum number of results to be returned per API request.
+
+    Methods:
+        build_query(author_contains: str = "", title_contains: str = "", year_from: int = None, year_to: int = None) -> str:
+            Builds an arXiv API search query using the specified parameters.
+        get_paper_list(query: str) -> List[Dict[str, str]]:
+            Fetches a list of arXiv preprints matching the specified query.
+        download_pdf(paper_id: str, save_dir: str) -> None:
+            Downloads the PDF for a specified arXiv preprint and saves it to the provided directory.
+        download_pdfs(paper_list: List[Dict[str, str]], save_dir: str) -> None:
+            Downloads the PDFs for a list of arXiv preprints and saves them to the provided directory.
+
+    Usage:
+        To use this class, instantiate an `ArxivScraper` object. Then, use the `build_query` method to create
+        a search query based on your desired parameters. Call the `get_paper_list` method with your query
+        to fetch a list of arXiv preprints that match your criteria. Finally, use the `download_pdfs` method
+        to download the PDFs of the listed preprints and save them to a specified directory.
+
+        Example:
+            scraper = ArxivScraper()
+            query = scraper.build_query(author_contains="Kania", title_contains="Banach", year_from=2010, year_to=2021)
+            paper_list = scraper.get_paper_list(query)
+            save_dir = "path/to/your/directory"
+            scraper.download_pdfs(paper_list, save_dir).
+    """
+
+    
     def __init__(self, max_results: int = 10):
         self.max_results = max_results
         self.base_url = "http://export.arxiv.org/api/query?"
